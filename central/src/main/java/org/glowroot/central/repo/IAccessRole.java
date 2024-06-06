b4d373ffb05256b1617aa230562dcd298d4ee2cf
Zeisler, Andrew (az9167) <az9167@us.att.com>
2024-06-06 14:17:01 -0500

    Adding IAccessRole

--- central/src/main/java/org/glowroot/central/repo/IAccessRole.java
+++ central/src/main/java/org/glowroot/central/repo/IAccessRole.java
@@ -4,7 +4,6 @@ import java.util.Set;
 
 public interface IAccessRole {
 
-    public Set<String> getPermissionsForRole(String roleType, String rollupName);
-    public String getAccessRoleType(String role);
-    public String getApplicationName(String role);
+    public Set<String> getPermissionsForRole(String roleName, Set<String> dbPermissions);
+
 }
